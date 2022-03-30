e49793f8e0f77ca97c8475329a1363a9f5d82a53
clases clases <clases@labinfo.is.escuelaing.edu.co>
2022-03-30 14:19:11 -0500

    Calculadora Terminada

---
+++ Calculadora/src/main/java/com/edu/eci/Calculadora/CalculadoraMain.java
@@ -0,0 +1,56 @@
+package com.edu.eci.Calculadora;
+
+
+import org.json.JSONObject;
+import spark.Request;
+import com.edu.eci.*;
+import spark.Response;
+
+import static spark.Spark.*;
+
+public class CalculadoraMain {
+
+    public static Funciones funciones = new Funciones();
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku");
+        get("/acos", (req, res) -> getRespuesta(req, res, "acos"));
+        get("/log", (req,res) -> getRespuesta(req, res, "log"));
+    }
+
+
+    /**
+     * Funcion que retorna un objeto JSON con el resultado de la operacion solicitada
+     * @param rec
+     * @param res
+     * @param operacion
+     * @return JsonObject ( "operation": "operacion", "input":  rec, "output":  resultado)
+     */
+
+    public static JSONObject getRespuesta(Request rec, Response res, String operacion){
+        JSONObject respuesta = new JSONObject();
+        Double valorACalcular = Double.valueOf(rec.queryParams("value"));
+        Double resultado;
+        if(operacion.equals("log")){
+            resultado = funciones.logaritmo(valorACalcular);
+        } else if(operacion.equals("acos")){
+            resultado = funciones.arcoseno(valorACalcular);
+        } else {
+            resultado = null;
+        }
+        respuesta.put("operation", operacion);
+        respuesta.put("input", valorACalcular);
+        respuesta.put("output", resultado);
+        return respuesta;
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
---
+++ Calculadora/src/main/java/com/edu/eci/Calculadora/Funciones.java
@@ -0,0 +1,11 @@
+package com.edu.eci.Calculadora;
+
+public class Funciones {
+    public Double logaritmo(double num){
+        return Math.log(num);
+    }
+    public Double arcoseno(double num){
+        return Math.acos(num);
+    }
+
+}
